b286d1ee31181578b4571ba97f5dd657cd42ac69
Henoc Dz <henocdz@gmail.com>
2014-10-30 16:19:12 -0600

    Base

---
+++ app/src/androidTest/java/com/where2go/where2go/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.where2go.where2go;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
